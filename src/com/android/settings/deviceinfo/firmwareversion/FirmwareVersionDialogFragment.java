70a36a9c6e949e0b093267b73e66a01442c0971c
Christian Oder <myself5@carbonrom.org>
2018-08-09 15:53:22 +0200

    Settings: Add Candy Version to FirmwareDialog
    * Show the Candy version in the Android version popup
    * Adapted from 3dec170 Create a dialog for firmware version
    
    Change-Id: I97dfbb070bacc5b8bc4c03751bef9f319a883cbc

--- src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionDialogFragment.java
+++ src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionDialogFragment.java
@@ -90,5 +90,6 @@ public class FirmwareVersionDialogFragment extends InstrumentedDialogFragment {
         new KernelVersionDialogController(this).initialize();
         new BuildNumberDialogController(this).initialize();
         new SELinuxStatusDialogController(this).initialize();
+        new CandyVersionDialogController(this).initialize();
     }
 }
